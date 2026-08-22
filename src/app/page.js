493c24eca4257c4d06a864b2dea3c08a96e33ed3
bennibeni <113261740+bennibeni@users.noreply.github.com>
2026-08-22 08:11:20 +0200

    projects-footer

--- src/app/page.js
+++ src/app/page.js
@@ -5,8 +5,8 @@ import SequenceSundaram from "./SequenceSundaram";
 
 export default function Page() {
   return (
-    <div className="flex min-h-full items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <div className="flex w-full max-w-7xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex min-h-screen items-start justify-center bg-zinc-50 font-sans dark:bg-black">
+      <div className="flex w-full max-w-7xl flex-col items-center justify-between bg-white px-4 py-8 dark:bg-black sm:items-start sm:px-16">
         <SequenceSundaram />
       </div>
     </div>
